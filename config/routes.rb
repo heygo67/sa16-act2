88311846e84c9750d8691d2e2481141fe76d23ac
McKenna Noakes <mckenna.noakes@outlook.com>
2024-04-29 14:54:35 -0500

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  root to: redirect('/home')
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'home', to: 'portfolio#home', as: 'home'
+  get 'about', to: 'portfolio#about', as: 'about'
+  get 'work', to: 'portfolio#work', as: 'work'
+  get 'contact', to: 'portfolio#contact', as: 'contact'
+
 end
